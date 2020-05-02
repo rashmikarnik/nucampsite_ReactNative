10e3d371466b36d872def6bc195f67ddd7c36330
Rashmi Karnik <rashmisag@gmail.com>
2020-05-01 19:24:36 -0700

    RN Modal

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 
 class Reservation extends Component {
@@ -10,7 +10,8 @@ class Reservation extends Component {
         this.state = {
             campers: 1,
             hikeIn: false,
-            date: ''
+            date: '',
+            showModal: false
         };
     }
 
@@ -18,79 +19,109 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
+    toggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
     handleReservation() {
         console.log(JSON.stringify(this.state));
+        this.toggleModal();
+    }
+
+    resetForm() {
         this.setState({
             campers: 1,
             hikeIn: false,
-            date: ''
+            date: '',
+            showModal: false
         });
     }
 
-    render() {
-        return (
-            <ScrollView>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Number of Campers</Text>
-                    <Picker
-                        style={styles.formItem}
-                        selectedValue={this.state.campers}
-                        onValueChange={itemValue => this.setState({ campers: itemValue })}>
-                        <Picker.Item label='1' value='1' />
-                        <Picker.Item label='2' value='2' />
-                        <Picker.Item label='3' value='3' />
-                        <Picker.Item label='4' value='4' />
-                        <Picker.Item label='5' value='5' />
-                        <Picker.Item label='6' value='6' />
-                    </Picker>
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Hike-In?</Text>
-                    <Switch
-                        style={styles.formItem}
-                        value={this.state.hikeIn}
-                        trackColor={{ true: "#5637DD", false: null }}
-                        onValueChange={value => this.setState({ hikeIn: value })}>
-                    </Switch>
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Date</Text>
-                    <DatePicker
-                        style={{flex: 2, marginRight: 20}}
-                        date={this.state.date}
-                        format='YYYY-MM-DD'
-                        mode='date'
-                        placeholder='Select Date'
-                        minDate={new Date().toISOString()}
-                        confirmBtnText='Confirm'
-                        cancelBtnText='Cancel'
-                        customStyles={{
-                            dateIcon: {
-                                position: 'absolute',
-                                left: 0,
-                                top: 4,
-                                marginLeft: 0
-                            },
-                            dateInput: {
-                                marginLeft: 36
-                            }
-                        }}
-                        onDateChange={date => {this.setState({date: date})}}
-                    />
-                </View>
-                <View style={styles.formRow}>
-                    <Button
-                        onPress={() => this.handleReservation()}
-                        title='Search'
-                        color='#5637DD'
-                        accessibilityLabel='Tap me to search for available campsites to reserve'
-                    />
-                </View>
-            </ScrollView>
 
-        );
+render() {
+    return (
+        <ScrollView>
+            <View style={styles.formRow}>
+                <Text style={styles.formLabel}>Number of Campers</Text>
+                <Picker
+                    style={styles.formItem}
+                    selectedValue={this.state.campers}
+                    onValueChange={itemValue => this.setState({ campers: itemValue })}>
+                    <Picker.Item label='1' value='1' />
+                    <Picker.Item label='2' value='2' />
+                    <Picker.Item label='3' value='3' />
+                    <Picker.Item label='4' value='4' />
+                    <Picker.Item label='5' value='5' />
+                    <Picker.Item label='6' value='6' />
+                </Picker>
+            </View>
+            <View style={styles.formRow}>
+                <Text style={styles.formLabel}>Hike-In?</Text>
+                <Switch
+                    style={styles.formItem}
+                    value={this.state.hikeIn}
+                    trackColor={{ true: "#5637DD", false: null }}
+                    onValueChange={value => this.setState({ hikeIn: value })}>
+                </Switch>
+            </View>
+            <View style={styles.formRow}>
+                <Text style={styles.formLabel}>Date</Text>
+                <DatePicker
+                    style={{ flex: 2, marginRight: 20 }}
+                    date={this.state.date}
+                    format='YYYY-MM-DD'
+                    mode='date'
+                    placeholder='Select Date'
+                    minDate={new Date().toISOString()}
+                    confirmBtnText='Confirm'
+                    cancelBtnText='Cancel'
+                    customStyles={{
+                        dateIcon: {
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0
+                        },
+                        dateInput: {
+                            marginLeft: 36
+                        }
+                    }}
+                    onDateChange={date => { this.setState({ date: date }) }}
+                />
+            </View>
+            <View style={styles.formRow}>
+                <Button
+                    onPress={() => this.handleReservation()}
+                    title='Search'
+                    color='#5637DD'
+                    accessibilityLabel='Tap me to search for available campsites to reserve'
+                />
+            </View>
+            <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.toggleModal()}>
+                    <View style={styles.modal}>
+                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
+                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
+                        <Text style={styles.modalText}>Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
+                        <Text style={styles.modalText}>Date: {this.state.date}</Text>
+                        <Button
+                            onPress={() => {
+                                this.toggleModal();
+                                this.resetForm();
+                            }}
+                            color='#5637DD'
+                            title='Close'
+                        />
+                    </View>
+                </Modal>
+        </ScrollView>
 
-    }
+    );
+
+}
 }
 
 const styles = StyleSheet.create({
@@ -107,6 +138,22 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
+    },
+    modal: { 
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#5637DD',
+        textAlign: 'center',
+        color: '#fff',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
     }
 });
 
